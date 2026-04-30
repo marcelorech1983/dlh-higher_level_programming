27c635d47ce430ccfa6ee43c104442b48e2c6520
Marcelo Rech <marcelorech@gmail.com>
2026-04-30 10:54:08 +0200

    Add task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if roman_string is None or type(roman_string) is not str:
+        return 0
     total = 0
     for i in range(len(roman_string)):
         current = roman[roman_string[i]]
