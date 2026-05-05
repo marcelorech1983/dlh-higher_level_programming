a6118ca71a181e8c0c867c06728c3ba49ae39500
Marcelo Rech <marcelorech@gmail.com>
2026-05-05 15:59:48 +0200

    Add task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module represent a class called Square"""
+
+
+class Square:
+    """This class represents a Square."""
+    def __init__(self, size):
+        """inicialize with size"""
+        self.__size = size
--- python-classes/README.md
+++ python-classes/README.md
@@ -3,7 +3,7 @@
 Python - Classes and Objects
 
 Task 0: Write an empty class Square that defines a square:
-Task 1: 
+Task 1: Write a class Square that defines a square by: (based on 0-square.py)
 Task 2: 
 Task 3: 
 Task 4: 
