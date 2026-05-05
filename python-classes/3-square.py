dbbf8a29821590c2f5cbea5620f3049bd6884773
Marcelo Rech <marcelorech@gmail.com>
2026-05-05 17:50:50 +0200

    Add task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -19,9 +19,9 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, size):
+    def size(self, value):
         """Define the size with validation"""
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
